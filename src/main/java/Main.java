b33fa20aa6a0b3f376c448bc1b3df4b3a10faf91
Mishator <voyteh89@mail.ru>
2023-06-14 13:35:03 +0300

    task 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,35 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-   private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-   private static final List<String> strings = new ArrayList<>(List.of("один", "два","два", "три", "три", "три"));
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
 
     public static void main(String[] args) {
         task1();
+        System.out.println();
+        task2();
     }
 
     public static void task1() {
         for (int num : nums) {
             if (num % 2 != 0) {
                 System.out.println(num);
+            }
+        }
+    }
+
+    public static void task2() {
+        Collections.sort(nums);
+
+        int prevNum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num % 2 == 0 && num != prevNum) {
+                System.out.println(num);
+                prevNum = num;
             }
         }
     }
