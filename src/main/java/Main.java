1e99fb69569232f72f7aef9ad628a2a49569c0be
Mishator <voyteh89@mail.ru>
2023-06-14 13:40:49 +0300

    task 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,16 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-    private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+    private static final List<String> words = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
 
     public static void main(String[] args) {
         task1();
         System.out.println();
         task2();
+        System.out.println();
+        task3();
     }
 
     public static void task1() {
@@ -32,6 +32,11 @@ public class Main {
                 prevNum = num;
             }
         }
+    }
+
+    public static void task3() {
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(uniqueWords);
     }
 
 }
